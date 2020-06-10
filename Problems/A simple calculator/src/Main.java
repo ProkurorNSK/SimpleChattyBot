1c567805b05f2a6ffe60e186cd1d22a1e629a286
ProkurorNSK <proka@ngs.ru>
2020-06-11 01:37:19 +0700

    Final2

---
+++ Problems/A simple calculator/src/Main.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        long a = scanner.nextLong();
+        String operator = scanner.next();
+        long b = scanner.nextLong();
+        switch (operator) {
+            case "+":
+                System.out.println(a + b);
+                break;
+            case "-":
+                System.out.println(a - b);
+                break;
+            case "*":
+                System.out.println(a * b);
+                break;
+            case "/":
+                if (b == 0) {
+                    System.out.println("Division by 0!");
+                } else {
+                    System.out.println(a / b);
+                }
+                break;
+            default:
+                System.out.println("Unknown operator");
+        }
+    }
+}
